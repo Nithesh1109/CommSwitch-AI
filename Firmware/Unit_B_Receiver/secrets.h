9b72eaa72314c10a00e66d914dd951ee97b0036f
Nithesh J <24040100529@reva.edu.in>
2026-05-11 21:11:14 +0530

    Add Unit B secrets template

---
+++ Firmware/Unit_B_Receiver/secrets.h
@@ -0,0 +1,10 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID       "YOUR_WIFI_NAME"
+#define WIFI_PASS       "YOUR_WIFI_PASSWORD"
+
+#define AIO_USERNAME    "YOUR_ADAFRUIT_USERNAME"
+#define AIO_KEY         "YOUR_ADAFRUIT_IO_KEY"
+
+#endif
